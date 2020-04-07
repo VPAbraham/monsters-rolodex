c0401d0238240f8d1728c46312d57bb62dcd3133
Victor Abraham <vpa4565@gmail.com>
2020-04-06 18:57:51 -0600

    Updates

---
+++ precache-manifest.d64359754a9713886b588df06deae70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee537ee5ab13b4b103e5b6c8f76fc5ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "92f0d58437aab68746a8",
+    "url": "/monsters-rolodex/static/css/main.559f1a51.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f0d58437aab68746a8",
+    "url": "/monsters-rolodex/static/js/main.1152c9c2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
